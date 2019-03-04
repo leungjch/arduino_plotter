e52f150a2d5ff352e2c54e24fb5d0bd07aed161d
Justin Leung <justin.leung.ch@gmail.com>
2019-03-04 05:50:11 -0500

    removed junk

--- gcodegenerator.py
+++ gcodegenerator.py
@@ -1,20 +1,20 @@
 from PIL import Image
 from numpy import*
 
-temp=Image.open('mac.png')
-size = 32,32
+temp=Image.open('turtle.png')
+size = 64,64
 temp.thumbnail(size, Image.ANTIALIAS)
 temp = temp.convert('1')
 temp.save('new.jpg', "PNG")
 
 A = array(temp)             # Creates an array, white pixels==True and black pixels==False
-new_A=empty((A.shape[0],A.shape[1]),None)    #New array with same size as A
+new_A=empty((A.shape[0],A.shape[1]),dtype=str)    #New array with same size as A
 
 for i in range(len(A)):
     for j in range(len(A[i])):
         if A[i][j]==True:
-            new_A[i][j]=0
+            new_A[i][j]="b"
         else:
-            new_A[i][j]=1
-
-savetxt("gcode.txt", new_A, delimiter=',',newline=',2,', fmt="%i")
+            new_A[i][j]="w"
+    
+savetxt("gcode.txt", new_A, delimiter=",", fmt="%s")
